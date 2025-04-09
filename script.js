b9ec96f8dc504c59408fc263076290231e9cee04
Sagar Vashist <sagarheromail10@gmail.com>
2025-04-09 13:49:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,31 @@
 let professionElement = document.getElementById('profession');
 let professions = ['Web Developer', 'Coder', 'Web Designer'];
 let currentProfessionIndex = 0;
-function changeProfession() {
-    professionElement.textContent = professions[currentProfessionIndex];
-    currentProfessionIndex = (currentProfessionIndex + 1) % professions.length;
+let charIndex = 0;
+
+function typeProfession() {
+    let currentProfession = professions[currentProfessionIndex];
+    if (charIndex < currentProfession.length) {
+        professionElement.textContent += currentProfession.charAt(charIndex);
+        charIndex++;
+        setTimeout(typeProfession, 100); 
+    } else {
+        setTimeout(eraseProfession, 1000); 
+    }
 }
-changeProfession();
-setInterval(changeProfession, 2000);
+
+function eraseProfession() {
+    if (charIndex > 0) {
+        professionElement.textContent = professionElement.textContent.slice(0, -1);
+        charIndex--;
+        setTimeout(eraseProfession, 50); 
+    } else {
+        currentProfessionIndex = (currentProfessionIndex + 1) % professions.length;
+        setTimeout(typeProfession, 500);
+    }
+}
+
+typeProfession();
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
